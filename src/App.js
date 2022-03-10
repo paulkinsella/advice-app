6dadc177329fbd155ac84ba930394b3e62cf1936
Paul Kinsella <kince23@gmail.com>
2022-03-10 20:40:24 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import divider from './images/divider.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const url = 'https://api.adviceslip.com/advice';
+
+  const getquote = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      }, []);
+  };
+
+  const id = data.slip ? data.slip.id : '';
+  const advice = data.slip ? data.slip.advice : 'Click button for some advice';
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='advice-card'>
+          <div className='quote-id'>
+            ADVICE #{id}
+          </div>
+          <div className='quote'>
+            {advice}
+          </div>
+          <div className='divider-container'>
+            <img className='divider' src={divider} alt='divider' />
+          </div>
+        </div>
+        <div className='button-container'>
+          <button className='advice-button' onClick={getquote}></button>
+        </div>
       </header>
     </div>
   );
